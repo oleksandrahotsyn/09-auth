013d462857fa5a9fec8e86884988d0b68d4e63cd
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-04-06 15:14:51 +0300

    add new files

---
+++ app/(auth routes)/sign-up/page.tsx
@@ -0,0 +1,34 @@
+<main className={css.mainContent}>
+  <h1 className={css.formTitle}>Sign up</h1>
+  <form className={css.form}>
+    <div className={css.formGroup}>
+      <label htmlFor="email">Email</label>
+      <input
+        id="email"
+        type="email"
+        name="email"
+        className={css.input}
+        required
+      />
+    </div>
+
+    <div className={css.formGroup}>
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        type="password"
+        name="password"
+        className={css.input}
+        required
+      />
+    </div>
+
+    <div className={css.actions}>
+      <button type="submit" className={css.submitButton}>
+        Register
+      </button>
+    </div>
+
+    <p className={css.error}>Error</p>
+  </form>
+</main>;
