75da37e1ffaeb2878852970c094345ef05e6e240
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-04-13 22:08:30 +0300

    instal react-hot-toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,59 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
-import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import TanstackProvider from "@/components/TanStackProvider/TanStackProvider";
+import AuthProvider from "@/components/AuthProvider/AuthProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
-  title: "App",
-  description: "next app",
+  title: "Note Hub",
+  description: "Create note hub",
   icons: {
-    icon: "./",
+    icon: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+  },
+  openGraph: {
+    title: "Note Hub",
+    description: "Create note hub",
+    url: "https://08-zustand-livid-omega.vercel.app/",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Note Hub",
+      },
+    ],
   },
 };
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>
-        <TanStackProvider>
-          <Header />
-          {children}
-        </TanStackProvider>
+    <html lang="en">
+      <body className={roboto.variable}>
+        <TanstackProvider>
+          <AuthProvider>
+            <Header />
+            <main>
+              {children}
+              {modal}
+            </main>
+            <Footer />
+          </AuthProvider>
+        </TanstackProvider>
       </body>
     </html>
   );
