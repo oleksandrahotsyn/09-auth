56608491a0b03f1b98ac3e909f6bded1ca78aa58
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-04-13 20:22:44 +0300

    add login

--- app/(auth routes)/sign-in/page.tsx
+++ app/(auth routes)/sign-in/page.tsx
@@ -2,49 +2,68 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { login, LoginRequest } from "@/lib/api/api";
-import { ApiError } from "@/app/api/api";
+import { useAuthStore } from "@/lib/store/authStore";
+import { login, RegisterRequest } from "@/lib/api/clientApi";
+import { ApiError } from "next/dist/server/api-utils";
 import css from "./SignInPage.module.css";
 
 const SignIn = () => {
   const router = useRouter();
   const [error, setError] = useState("");
-  const handleSubmit = async (formData: FormData)
-    
-return(
-<main className={css.mainContent}>
-    <form className={css.form}>
-      <h1 className={css.formTitle}>Sign in</h1>
+  const setUser = useAuthStore((state) => state.setUser);
 
-      <div className={css.formGroup}>
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          type="email"
-          name="email"
-          className={css.input}
-          required
-        />
-      </div>
+  const handleSubmit = async (formData: FormData) => {
+    try {
+      const formValues: RegisterRequest = {
+        email: formData.get("email") as string,
+        password: formData.get("password") as string,
+      };
+      const res = await login(formValues);
+      if (res) {
+        setUser(res);
+        router.push("/profile");
+      } else {
+        setError("Invalid email or password");
+      }
+    } catch (error) {
+      setError((error as ApiError).message ?? "Oops... some error");
+    }
+  };
+  return (
+    <main className={css.mainContent}>
+      <form className={css.form}>
+        <h1 className={css.formTitle}>Sign in</h1>
 
-      <div className={css.formGroup}>
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          name="password"
-          className={css.input}
-          required
-        />
-      </div>
+        <div className={css.formGroup}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            className={css.input}
+            required
+          />
+        </div>
 
-      <div className={css.actions}>
-        <button type="submit" className={css.submitButton}>
-          Log in
-        </button>
-      </div>
+        <div className={css.formGroup}>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            className={css.input}
+            required
+          />
+        </div>
 
-      <p className={css.error}>{error}</p>
-    </form>
-    </main>);
-}
+        <div className={css.actions}>
+          <button type="submit" className={css.submitButton}>
+            Log in
+          </button>
+        </div>
+
+        <p className={css.error}>{error}</p>
+      </form>
+    </main>
+  );
+};
--- lib/api/api.ts
+++ lib/api/api.ts
@@ -24,3 +24,13 @@ export const register = async (data: RegisterRequest) => {
   const res = await nextServer.post<User>("/auth/register", data);
   return res.data;
 };
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export const login = async (data: LoginRequest) => {
+  const res = await nextServer.post<User>("/auth/login", data);
+  return res.data;
+};
