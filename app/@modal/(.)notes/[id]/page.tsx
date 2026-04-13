8cab726a4f3d9f5d5df131900fa4ce04a59820c4
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-04-13 22:01:18 +0300

    install use-debounce

---
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -0,0 +1,30 @@
+import { fetchNoteByIdServer } from "@/lib/api/serverApi";
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
+import NotePreview from "./NotePreview.client";
+
+type Props = {
+  // params: { id: string };
+  params: Promise<{ id: string }>;
+};
+
+const NoteDetails = async ({ params }: Props) => {
+  const { id } = await params;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteByIdServer(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotePreview id={id} />
+    </HydrationBoundary>
+  );
+};
+
+export default NoteDetails;
