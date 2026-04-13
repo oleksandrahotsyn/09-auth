9083cebad07948b3d9e72e6f3cccb380283b5e65
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-04-13 21:58:31 +0300

    fix edit profile

--- app/(private routes)/profile/edit/page.tsx
+++ app/(private routes)/profile/edit/page.tsx
@@ -1,33 +1,92 @@
-import css from "../profilePage.module.css";
+"use client";
 
-<main className={css.mainContent}>
-  <div className={css.profileCard}>
-    <h1 className={css.formTitle}>Edit Profile</h1>
+import Image from "next/image";
+import css from "./EditProfilePage.module.css";
+import { getMe, updateUserProfile } from "@/lib/api/clientApi";
+import toast, { Toaster } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { useAuthStore } from "@/lib/store/authStore";
+import { useEffect, useState } from "react";
 
-    <img
-      src="avatar"
-      alt="User Avatar"
-      width={120}
-      height={120}
-      className={css.avatar}
-    />
+const EditProfilePage = () => {
+  const [userName, setUserName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const setUser = useAuthStore((state) => state.setUser);
+  const router = useRouter();
 
-    <form className={css.profileInfo}>
-      <div className={css.usernameWrapper}>
-        <label htmlFor="username">Username:</label>
-        <input id="username" type="text" className={css.input} />
+  useEffect(() => {
+    getMe().then((user) => {
+      setUserName(user.username ?? "");
+      setEmail(user.email ?? "");
+    });
+  }, []);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(event.target.value);
+  };
+
+  const handleSaveUser = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (userName.trim().length === 0) {
+      toast.error("Please write your username");
+      return;
+    }
+    if (userName.trim().length > 20) {
+      toast.error("Username cannot exceed 20 characters");
+      return;
+    }
+    const res = await updateUserProfile({ username: userName });
+    if (res) {
+      setUser(res);
+      router.push("/profile");
+      toast.success("Successfully edit");
+    }
+  };
+  return (
+    <main className={css.mainContent}>
+      <div className={css.profileCard}>
+        <h1 className={css.formTitle}>Edit Profile</h1>
+
+        <Image
+          src="https://ac.goit.global/fullstack/react/default-avatar.jpg"
+          alt="User Avatar"
+          width={120}
+          height={120}
+          className={css.avatar}
+        />
+
+        <form className={css.profileInfo} onSubmit={handleSaveUser}>
+          <div className={css.usernameWrapper}>
+            <label htmlFor="username">Username:</label>
+            <input
+              id="username"
+              type="text"
+              value={userName}
+              className={css.input}
+              onChange={handleChange}
+            />
+          </div>
+
+          <p>Email:{email}</p>
+
+          <div className={css.actions}>
+            <button type="submit" className={css.saveButton}>
+              Save
+            </button>
+            <button
+              type="button"
+              className={css.cancelButton}
+              onClick={router.back}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
       </div>
+      <Toaster />
+    </main>
+  );
+};
 
-      <p>Email: user_email@example.com</p>
-
-      <div className={css.actions}>
-        <button type="submit" className={css.saveButton}>
-          Save
-        </button>
-        <button type="button" className={css.cancelButton}>
-          Cancel
-        </button>
-      </div>
-    </form>
-  </div>
-</main>;
+export default EditProfilePage;
--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,16 +1,44 @@
-import css from "./profilePage.module.css";
+import { getMeServer } from "@/lib/api/serverApi";
+import { Metadata } from "next";
+import Link from "next/link";
+import css from "./ProfilePage.module.css";
+import Image from "next/image";
 
-export default async function Profile() {
+export const metadata: Metadata = {
+  title: "Profile | Note Hub",
+  description: "Profile View Page",
+  icons: {
+    icon: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+  },
+  openGraph: {
+    title: "Profile page",
+    description: "Profile View Page",
+    url: "https://08-zustand-livid-omega.vercel.app/",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Note Hub",
+      },
+    ],
+  },
+};
+const ProfilePage = async () => {
+  const user = await getMeServer();
+
   return (
     <main className={css.mainContent}>
       <div className={css.profileCard}>
         <div className={css.header}>
           <h1 className={css.formTitle}>Profile Page</h1>
-          <a className={css.editProfileButton}>Edit Profile</a>
+          <Link href="/profile/edit" className={css.editProfileButton}>
+            Edit Profile
+          </Link>
         </div>
         <div className={css.avatarWrapper}>
-          <img
-            src="Avatar"
+          <Image
+            src={user.avatar}
             alt="User Avatar"
             width={120}
             height={120}
@@ -18,10 +46,12 @@ export default async function Profile() {
           />
         </div>
         <div className={css.profileInfo}>
-          <p>Username: your_username</p>
-          <p>Email: your_email@example.com</p>
+          <p>Username: {user.username}</p>
+          <p>Email: {user.email}</p>
         </div>
       </div>
     </main>
   );
-}
+};
+
+export default ProfilePage;
